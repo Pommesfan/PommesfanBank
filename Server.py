d626b7b9f663e234394c18b5ae8b09ac105bb0cd
Johannes Wirbser <johannes.wirbser@gmx.de>
2022-07-02 17:47:57 +0200

    database introduced

--- Server.py
+++ Server.py
@@ -1,27 +1,63 @@
 import socket
+import sqlite3
+
 from Utils import *
 
 localIP = "127.0.0.1"
 localPort = 20001
 
 max_amount = 2000
-customers = [
-    ("45321695", "AAAA", "hallo", 6598),
-    ("15369754", "BBBB", "hi", 9832),
-    ("12498625", "CCCC", "ups", 4682),
-    ("49871283", "DDDD", "jesses", 361)
-]
+
+con = sqlite3.connect("Hallo.db")
 
 
-def get_customer_index(username, password):
-    for i in range(len(customers)):
-        c = customers[i]
-        if c[1] == username:
-            if c[2] == password:
-                return i
-            else:
-                return -1
-    return -1
+def error(s):
+    con.close()
+    print(s)
+    exit(1)
+
+
+def init_database():
+    con.execute("create table customer(customer_id, customer_name, password, balance)")
+    con.commit()
+
+    customers = [
+        ("45321695", "AAAA", "hallo", 6598),
+        ("15369754", "BBBB", "hi", 9832),
+        ("12498625", "CCCC", "ups", 4682),
+        ("49871283", "DDDD", "jesses", 361)
+    ]
+    for c in customers:
+        con.execute("insert into customer values ('" + c[0] + "', '" + c[1] + "', '" + c[2] + "', " + str(c[3]) + ")")
+    con.commit()
+
+
+#init_database()
+
+
+def get_customer_id(username, password):
+    response = con.execute("select * from customer where customer_name = '" + username + "'")
+    answer = None
+    for r in response:
+        answer = r
+        break
+
+    if answer is None or answer[2] != password:
+        return None
+    else:
+        return answer[0]
+
+
+def query_balance(customer_id):
+    response = con.execute("select balance from customer where customer_id = '" + customer_id + "'")
+    answer = None
+    for r in response:
+        answer = r
+        break
+    if answer is None:
+        error("Abfrage mit nicht existierender Kunden-ID")
+    else:
+        return answer[0]
 
 
 UDPServerSocket = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
@@ -38,11 +74,10 @@ while True:
     end_of_password = start_of_password + length_of_password
     password = paket[start_of_password:end_of_password].decode(UTF8STR)
 
-    customer_index = get_customer_index(username, password)
-    if customer_index < 0:
-        balance = 0
+    customer_id = get_customer_id(username, password)
+    if customer_id is None:
         print("Fehllogin: Nutzer: " + username)
     else:
         print("Kunde: " + username + " hat Kontostand abgefragt")
-        balance = customers[customer_index][3]
-    UDPServerSocket.sendto(int_to_bytes(balance), src)
+        balance = query_balance(customer_id)
+        UDPServerSocket.sendto(int_to_bytes(balance), src)
