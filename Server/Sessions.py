a8b4705b38ef8ea0d8a6ae7feea6b3920cd5cf98
Johannes Wirbser <johannes.wirbser@gmx.de>
2022-09-19 19:50:11 +0200

    replace running session by relogin

--- Server/Sessions.py
+++ Server/Sessions.py
@@ -15,10 +15,14 @@ class SessionList:
         self.__lock = Lock()
 
     def add(self, session):
-        self.__lock.acquire()
         if isinstance(session, Session):
-            self.__sessions.append(session)
-        self.__lock.release()
+            session_with_same_customer = self.get_session_to_customer(session.customer_id)
+            self.__lock.acquire()
+            if session_with_same_customer is not None:
+                self.__sessions[session_with_same_customer] = session
+            else:
+                self.__sessions.append(session)
+            self.__lock.release()
 
     def get_session_from_id(self, session_id, src):
         self.__lock.acquire()
@@ -38,3 +42,12 @@ class SessionList:
                 return 0
         self.__lock.release()
         return -1
+
+    def get_session_to_customer(self, customer_id):
+        self.__lock.acquire()
+        for i in range(len(self.__sessions)):
+            if self.__sessions[i].customer_id == customer_id:
+                self.__lock.release()
+                return i
+        self.__lock.release()
+        return None
